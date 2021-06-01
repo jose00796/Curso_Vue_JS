ea34e33942e6dc262fcbe232bd238727f52f1c13
jose00796 <josebalboa11@hotmail.com>
2021-06-01 15:28:38 -0400

    Inicio, Introduccion Vue_JS

---
+++ 01.js
@@ -0,0 +1,14 @@
+const app = new Vue({
+    el: "#app",
+    data:{
+        titulo : "Redmi Go",
+        libros : ['Mateo', 'Marcos', 'Lucas', 'Juan', 'Josue'],
+        personas : [
+            {nombre: 'Migdaly', edad : 22},
+            {nombre: 'Franmy', edad : 22},
+            {nombre: 'Ibelise', edad : 19},
+            {nombre: 'Jose Andres', edad : 25},
+            
+        ]
+    }
+});
